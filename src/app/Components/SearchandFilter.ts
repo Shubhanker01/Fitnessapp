94ce2cbceefd0b05f7ec9268381e489982a3984b
Shubhanker01 <shubhanker40@gmail.com>
2024-07-15 17:59:11 +0530

    commit3

---
+++ src/app/Components/SearchandFilter.ts
@@ -0,0 +1,8 @@
+// import { Component } from "@angular/core";
+
+// @Component({
+//     standalone:true,
+//     selector:'search-and-filter',
+//     templateUrl:'search.html'
+// })
+// export class SearchandFilter{}

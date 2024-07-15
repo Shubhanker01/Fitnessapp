9e191d1da8820d1bc3dc512d76fadc6a64419937
Shubhanker01 <shubhanker40@gmail.com>
2024-07-15 20:39:16 +0530

    commit4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { InputFormComponent } from './input-form/input-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  // templateUrl: `./app.component.html`,
-  template: `<router-outlet></router-outlet>
-     <app-home></app-home>`,
+  imports: [RouterOutlet, HomeComponent, InputFormComponent],
+  template: `
+    <main class="main">
+    <app-input-form></app-input-form>
+    <app-home></app-home>
+    </main>
+
+    <router-outlet/>
+  `,
   styleUrl: './app.component.css'
 })
 
